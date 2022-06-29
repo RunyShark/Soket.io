e9733ef5aecbfd2d51b1acf1930d72aef6817c40
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-06-29 17:46:03 -0500

    evento

--- public/js/escritorio.js
+++ public/js/escritorio.js
@@ -1,3 +1,24 @@
+const escritorioNombre = document.querySelector("h1");
+const btnButtom = document.querySelector("buttom");
 
+const socket = io();
 
-console.log('Escritorio HTML');
+const searchPArams = new URLSearchParams(window.location.search);
+
+if (!searchPArams.has("escritorio")) {
+  window.location = "index.html";
+  throw new Error(`El escritorio es obligatorio`);
+}
+
+const escritorio = searchPArams.get("escritorio");
+escritorioNombre.innerText = escritorio;
+
+socket.on("connect", () => {
+  btnButtom.disable = false;
+});
+
+socket.on("disconnect", () => {
+  btnButtom.disable = true;
+});
+
+btnButtom.addEventListener("click", () => {});
